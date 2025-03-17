8dfb1a67832a0513211f3ed675d3eb65ed5ccc20
Thaw Zin Soe <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2025-03-17 14:54:41 +0630

    Remove vendor

--- Modules/Vendor/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('vendor::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('vendor.name') !!}
-    </p>
-@endsection
